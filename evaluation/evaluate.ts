8c37638fe940d2eb1d328b2db630358d4f1acfbb
Midhun A Darvin <midhunadarvin@gmail.com>
2021-05-16 17:52:10 +0530

    - add comments - fix issue

--- evaluation/evaluate.ts
+++ evaluation/evaluate.ts
@@ -1,17 +1,23 @@
 import { FUNCTIONS } from '../ast/FUNCTIONS';
 import { NODE_TYPE } from '../ast/NODE_TYPE';
 
+// The main evaluate function
 export function evaluate(ast) {
   if (ast && ast.type !== 'Program') {
     return TypeError('Error in evaluation: Unknown object ' + ast);
   }
   const result = [];
   for (let child of ast.body) {
+    // For each of the children in the body of the ast
+    // we will call evaluateExpression to evaluate what type of
+    // node it is.
     result.push(evaluateExpression(child));
   }
   return result;
 }
 
+// In this function we will evaluate what node it is and then
+// pass the node to it's corresponding handler
 function evaluateExpression(node) {
   switch (node.type) {
     case NODE_TYPE.NUMBER: {
@@ -29,16 +35,18 @@ function evaluateExpression(node) {
   }
 }
 
+// For a number literal node, we simply return it's value
 function evaluateNumberLiteral(node) {
-  // For a number literal node, we simply return it's value
   return Number(node.value);
 }
 
+// For a string literal node, we simply return it's value
 function evaluateStringLiteral(node) {
-  // For a string literal node, we simply return it's value
   return node.value;
 }
 
+// For a call expression node, we first check what function call
+// is to be executed, and then call it's corresponding handler.
 function evaluateCallExpression(node) {
   switch (node.name) {
     case FUNCTIONS.add: {
@@ -53,6 +61,9 @@ function evaluateCallExpression(node) {
   }
 }
 
+// In add function expression : add (2, 3)
+// We first evaluate the parameters, and then add those
+// values together.
 function evaluateAddFunction(node) {
   if (!node || !node.params || node.params.length === 0) {
     return SyntaxError('Missing Parameters for add function ' + node);
@@ -68,6 +79,9 @@ function evaluateAddFunction(node) {
   }, 0);
 }
 
+// In subtract function expression : add (5, 4)
+// We first evaluate the parameters, and then subtract
+// the values from the first parameter
 function evaluateSubtractFunction(node) {
   if (!node || !node.params || node.params.length === 0) {
     return SyntaxError('Missing Parameters for subtract function ' + node);
@@ -85,9 +99,15 @@ function evaluateSubtractFunction(node) {
 
   return result;
 }
-
+// Here we evaluate whether the give
 function evaluateIsNumFunction(node) {
-  return !isNaN(Number(node.value));
+  for (let i = 0; i < node.params.length; i++) {
+    const param = evaluateExpression(node.params[i]);
+    if (isNaN(Number(param))) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function evaluateIfCondition(node) {
